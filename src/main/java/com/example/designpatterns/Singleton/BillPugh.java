8ee888e09ba040b44a0687a694c55a343bdaf571
shahdkhadr <skhader@asaltech.com>
2024-10-10 11:06:47 +0300

    add the bill pugh

---
+++ src/main/java/com/example/designpatterns/Singleton/BillPugh.java
@@ -0,0 +1,13 @@
+package com.example.designpatterns.Singleton;
+
+public class BillPugh {
+    private BillPugh(){
+
+    }
+    private static class SingletonHelper{
+        private static final BillPugh INSTANCE = new BillPugh();
+    }
+    public static BillPugh getInstance() {
+        return SingletonHelper.INSTANCE;
+    }
+}
